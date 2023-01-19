44f58d8297b1508e76dbf9e25cae84ed9037ca2b
TheRootSixtyFour <TheRootSixtyFour@gmail.com>
2023-01-19 13:26:03 +0100

    count characters with JS again

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const textareaEl = document.getElementById("textarea");
+const totalCounterEl = document.getElementById("total-counter");
+const remainingCounterEl = document.getElementById("remaining-counter");
 
 textareaEl.addEventListener("keyup", () => {
-    console.log("key pressed");
+    updateCounter()
 })
+
+updateCounter();
+
+function updateCounter() {
+    totalCounterEl.innerText = textareaEl.value.length;
+    remainingCounterEl.innerText = textareaEl.getAttribute("maxLength") - totalCounterEl.innerText ;
+}
